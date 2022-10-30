08ebe21137c5d6edd1717a8ae15c5e8d6fdbe730
Vikrant Sharma <projects@systemarchs.com>
2022-10-30 14:16:18 +0530

    30-0ct ssh test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const encrypt = require('./encrypt');
 const decrypt = require('./decrypt');
 
-
+   //rrijfijfirj
 const publicKey = fs.readFileSync(__dirname + '/id_rsa_pub.pem', 'utf8');
 const encryptedMessage = encrypt.encryptWithPublicKey(publicKey, 'Super');
 console.log(encryptedMessage.toString());
